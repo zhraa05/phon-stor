d218c7456c18570225cb55a422d45e635dd387dc
zhraa2 <zhraazhraahgz5@gmail.com>
2022-04-01 17:13:45 +0800

    first commit

---
+++ js/min.js
@@ -0,0 +1,43 @@
+// let minclor =localStorage.getItem("color-option")
+// console.log(minclor)
+// if (minclor !== null){
+//    document.documentElement.style.setProperty('--min-color' , localStorage.getItem("color-option"));
+// }
+
+
+
+
+
+
+
+
+// document.querySelector(".togel-setting .fa-cog").onclick = function(){
+//     this.classList.toggle("fa-spin")
+//     document.querySelector(".setting-box").classList.toggle("opn")
+// }
+
+// const colorli = document.querySelectorAll(".color li");
+
+//  colorli.forEach(li => {
+  
+//      li.addEventListener( "click" , (e) => { 
+//          document.documentElement.style.setProperty('--min-color' , e.target.dataset.color);
+//         localStorage.setItem("color-option" , e.target.dataset.color)
+//         e.target.parentElement.querySelector(".activ").forEach(ele => {
+//             ele.classList.remove("activ");
+//         })
+//         e.target.classList.add("activ");
+//      });
+//  })
+
+
+// let landBage = document.querySelector(".landing-page")
+
+// let imgArray = ["preview.jpg", "images (1).jpg", "download (2).jpg", "images (1).jpg", "download (1).jpg"]
+
+// setInterval(() => {
+
+//     let randam = Math.floor(Math.random() * imgArray.length)
+
+// landBage.style.backgroundImage = 'url("img/' + imgArray[randam] +   '")';
+// },8000)
